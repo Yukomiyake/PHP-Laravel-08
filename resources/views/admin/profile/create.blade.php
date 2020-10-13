264979c8de071935614ff3fa8c7d64a68f07656c
yuko <kyonan_yuko@yahoo.co.jp>
2020-10-13 01:36:07 +0000

    11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,3 +11,20 @@
         <h1>Profile作成画面</h1>
     </body>
 </html>
+ {{-- layouts/profile.blade.phpを読み込む --}}
+    @extends('layouts.profile')
+
+
+    {{-- admin.profile.phpの@yield('title')に'profileの新規作成'を埋め込む --}}
+    @section('title', 'profileの新規作成')
+
+    {{-- admin.profile.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>profile</h2>
+            </div>
+        </div>
+    </div>
+    @endsection
