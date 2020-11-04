5a1c8bd0ec7136715096fbcb7867bc39d26113e7
yuko <kyonan_yuko@yahoo.co.jp>
2020-11-04 07:30:41 +0000

    PHP/Laravel12

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <mate name="viewport" content="width=device-width,initial-scale=1">
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
 
-        <title>Profile</title>    
-    </head>
-    <body>
-        <h1>Profile編集画面</h1>
-    </body>
-</html>
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title','プロフィールの編集')
+
+{{-- admin.plade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+              <h2>プロフィールの編集</h2>
+            </div>
+        </div>
+    </div> 
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create','Admin\NewsController@add');
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
     Route::get('news/create','Admin\NewsController@add')->middleware('auth');
 
 });
@@ -28,3 +27,4 @@ Route::group(['prefix' => 'admin'], function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
